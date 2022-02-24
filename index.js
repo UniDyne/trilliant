39eabdc564f926b4c675e96d45c211ba8d36f86a
UniDyne <unidyne@gmail.com>
2022-02-24 13:57:58 -0500

    Added JWT export

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ module.exports = {
 
     ServiceWrapper: require('./src/services/ServiceWrapper'),
     WebService: require('./src/services/WebService'),
-    SQLService: require('./src/services/SQLService')
+    SQLService: require('./src/services/SQLService'),
+
+
+    JWT: require('./src/httpd/RFC7519.js')
 };
