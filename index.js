0ce4d79e4502b64c16109f8029885e9bd91deca1
D. Oliver Brown <unidyne@gmail.com>
2026-07-21 01:53:10 -0400

    Fix export

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { Template } = require('./lib/Template');
 const Application = require('./src/Application');
 const Plugin = require('./src/Plugin');
 const { APIPlugin } = require('./src/APIPlugin');
-const { MessageEnvelope } = require('./src/MessageEnvelope');
-const { TokenHandler } = require('./src/TokenHandler');
+const MessageEnvelope = require('./src/MessageEnvelope');
+const TokenHandler = require('./src/TokenHandler');
 
 const Service = require('./src/Service');
 const ServiceWrapper = require('./src/ServiceWrapper');
